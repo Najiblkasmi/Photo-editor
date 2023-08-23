778bf3d0dee91c78a4c337612142b6fbed81c228
Najiblkasmi <143021252+Najiblkasmi@users.noreply.github.com>
2023-08-23 19:13:28 +0200

    Add files via upload

---
+++ photoEditor.py
@@ -0,0 +1,29 @@
+from PIL import Image, ImageFilter, ImageEnhance
+import os
+
+# Update the source and target paths to the correct absolute paths on your system
+path = r'C:\Users\najib\Desktop\Python projects\py\imgs'
+pathOut = r'C:\Users\najib\Desktop\Python projects\py\editedImgs'
+
+# Create the target directory if it doesn't exist
+if not os.path.exists(pathOut):
+    os.makedirs(pathOut)
+
+# Loop through each file in the source directory
+for filename in os.listdir(path):
+    if filename.endswith('.jpg'):  # Only process JPG files (you can adjust the file extension)
+        img = Image.open(os.path.join(path, filename))
+
+        # Apply the sharpen filter
+        edit = img.filter(ImageFilter.SHARPEN).convert('L')
+        
+        factor= 1.5
+        enhancer = ImageEnhance.Contrast(edit)
+        clean_name = os.path.splitext(filename)[0]
+
+        # Save the edited image to the target directory
+        edited_filename = f'{clean_name}_edited.jpg'
+        edited_path = os.path.join(pathOut, edited_filename)
+        edit.save(edited_path)
+
+print("Image processing complete.")
